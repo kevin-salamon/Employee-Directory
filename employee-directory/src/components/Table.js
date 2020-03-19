0036f80ef6e6968688c6a30fef99a4b9307cd780
kevin-salamon <kevinwebdevs@gmail.com>
2020-03-19 17:53:59 -0400

    completed basic table structure

--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -1,10 +1,31 @@
 import React from "react";
+import employees from "../employees.json";
 
-// By extending the React.Component class, Counter inherits functionality from it
-class Table extends React.Component {
-  render() { return(
-    <h1>Test</h1>
-  )}
+function Table(props) {
+  return (
+    <table>
+      <tr>
+        <th>Employee ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+      </tr>
+      {
+        employees.map((employee) =>
+        <tr>
+          <td>{employee.id}</td>
+          <td>{employee.firstname}</td>
+          <td>{employee.lastname}</td>
+          <td>{employee.role}</td>
+        </tr>)
+      }
+    </table>
+  );
 }
 
 export default Table;
+
+// itemNot.map((item) => 
+// <li className="list-group-item" key={item.id}>
+//   {item.name}
+// </li>)
