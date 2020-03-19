25b965a5200da7664c4703b24f8990b917bb130c
kevin-salamon <kevinwebdevs@gmail.com>
2020-03-19 17:11:25 -0400

    basic react app structure created

---
+++ employee-directory/src/components/Table.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// By extending the React.Component class, Counter inherits functionality from it
+class Table extends React.Component {
+  render() { return(
+    <h1>Test</h1>
+  )}
+}
+
+export default Table;
