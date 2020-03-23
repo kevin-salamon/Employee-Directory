3d744979a606b3695e3f08e0f1a56cab9a6a3d58
kevin-salamon <kevinwebdevs@gmail.com>
2020-03-23 16:02:36 -0400

    Updates

---
+++ precache-manifest.c9eb9b0273e6ecbb66e49b39d1831012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d9478623949913469a3fa38c895b7e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5127ad2d790adcbb968c",
+    "url": "/my-app/static/css/main.e43b42b2.chunk.css"
+  },
+  {
+    "revision": "1f07cbc89e7045ab2da6",
+    "url": "/my-app/static/js/2.fcf30f28.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.fcf30f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5127ad2d790adcbb968c",
+    "url": "/my-app/static/js/main.fd0d816a.chunk.js"
+  },
+  {
+    "revision": "20b26ff37ec4a75f51a8",
+    "url": "/my-app/static/js/runtime-main.7c62e6bd.js"
+  }
+]);
